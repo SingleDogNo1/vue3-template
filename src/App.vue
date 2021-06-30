3b9548d955278a75b1da3b2dae6dd5f76597924c
lost-dream <13623673118@163.com>
2021-06-30 10:03:25 +0800

    refactor: 添加stylelint，执行格式化

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ export default defineComponent({
 
 <style>
 #app {
+  margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
 }
 </style>
